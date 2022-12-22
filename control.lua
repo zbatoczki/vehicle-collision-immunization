4e1d4ace9c72bba12d8929f04d0c2b742b2e8732
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2022-12-21 17:53:24 -0800

    Added entity check for health damage negation

--- control.lua
+++ control.lua
@@ -1,12 +1,22 @@
-local function preventDamage(event)
-    event.entity.health = event.entity.prototype.max_health
-  end
+local function preventDamage(event)    
+    local causeEntity = event.cause
+    local targetEntity = event.entity
+
+    if causeEntity and targetEntity then
+        if event.entity.force.name == "player" and event.cause.type == "car" then
+            event.entity.health = event.entity.prototype.max_health
+        end
+    end
+end
   
-  -- Register the on_collision event to run the on_collision function when a collision occurs
-  local on_entity_damaged_filter = {
+local on_entity_damaged_filter = {
     {
         filter = "damage-type",
         type = "impact"
     }
+    ,{
+        filter = "type",
+        type = "car"
+    }
   }
-  script.on_event(defines.events.on_entity_damaged , preventDamage, on_entity_damaged_filter)
+script.on_event(defines.events.on_entity_damaged , preventDamage, on_entity_damaged_filter)
